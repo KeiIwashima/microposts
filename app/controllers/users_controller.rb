1c71157c941d8c8a684b665b3e0035d395aa7fbf
keiiwashima <uniks.empire@gmail.com>
2016-01-18 12:23:45 +0000

    User-Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   
   def show
     @user = User.find(params[:id])
@@ -18,10 +19,30 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to user_path
+    end
+  end
+    #自分のユーザー(current_user)編集ページ(users/:id/edit)ではない場合TOPページへリダイレクト
+  
+  def update
+    @user = User.find(params[:id])
+       if @user.update_attributes(user_params)
+         flash[:success] = "Profileを更新しました"
+        # Updateに成功した場合はprofileページへリダイレクト
+        redirect_to user_path
+      else
+        # Updateに失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private 
   
   def user_params
      params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+                                 :password_confirmation, :location, :about)
+   end 
 end
