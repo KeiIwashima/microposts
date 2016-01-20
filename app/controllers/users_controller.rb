e0b996c7e33d8f9a6205ea78ebb8c32e6d7ac6b8
keiiwashima <uniks.empire@gmail.com>
2016-01-20 11:18:46 +0000

    user-profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:update]
+ before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
     if current_user != @user
       redirect_to root_path
     end
@@ -29,7 +28,6 @@ class UsersController < ApplicationController
     #自分のユーザー(current_user)編集ページ(users/:id/edit)ではない場合TOPページへリダイレクト
   
   def update
-    @user = User.find(params[:id])
      if @user.update(user_params)
          flash[:success] = "Profileを更新しました"
         # Updateに成功した場合はprofileページへリダイレクト
@@ -46,4 +44,10 @@ class UsersController < ApplicationController
        params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :location, :about)
    end
-  end
+  
+   def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) if current_user != @user
+   end
+   
+end
