30948f3df53e459cff3e2842e56b76c4838886b0
keiiwashima <uniks.empire@gmail.com>
2016-02-11 04:51:10 +0000

    updated

--- app/controllers/micropostrelationships_controller.rb
+++ app/controllers/micropostrelationships_controller.rb
@@ -1,6 +1,6 @@
 class MicropostrelationshipsController < ApplicationController
-    before_action :logged_in_user
-
+   before_action :logged_in_user
+   
   def create
     @micropost = Micropost.find(params[:micropost_id])
     current_user.iine(@micropost)
